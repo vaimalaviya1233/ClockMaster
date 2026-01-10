75f7719ef36439388de34d1cfc7717dbcce76923
PranshulGG <pranshul.devmain@gmail.com>
2026-01-10 14:15:21 +0530

    updated alarm activity UI logic closes #11

--- app/src/main/java/com/pranshulgg/clockmaster/AlarmActivity.kt
+++ app/src/main/java/com/pranshulgg/clockmaster/AlarmActivity.kt
@@ -44,27 +44,6 @@ class AlarmActivity : AppCompatActivity() {
         )
         wakeLock.acquire(10 * 60 * 1000L)
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-            WindowCompat.setDecorFitsSystemWindows(window, false)
-
-            window.insetsController?.let { controller ->
-                controller.hide(WindowInsets.Type.statusBars() or WindowInsets.Type.navigationBars())
-                controller.systemBarsBehavior =
-                    WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
-            }
-
-        } else {
-            // idk why this happened
-            @Suppress("DEPRECATION")
-            window.decorView.systemUiVisibility =
-                (View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                        or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                        or View.SYSTEM_UI_FLAG_LAYOUT_STABLE)
-            @Suppress("DEPRECATION")
-            window.statusBarColor = android.graphics.Color.TRANSPARENT
-            @Suppress("DEPRECATION")
-            window.navigationBarColor = android.graphics.Color.TRANSPARENT
-        }
 
 //
 //        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O_MR1) {
@@ -96,6 +75,31 @@ class AlarmActivity : AppCompatActivity() {
 
 
         setContentView(R.layout.activity_alarm)
+
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            WindowCompat.setDecorFitsSystemWindows(window, false)
+
+            window.decorView.post {
+                window.insetsController?.let { controller ->
+                    controller.hide(
+                        WindowInsets.Type.statusBars() or WindowInsets.Type.navigationBars()
+                    )
+                    controller.systemBarsBehavior =
+                        WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+                }
+            }
+        } else {
+            @Suppress("DEPRECATION")
+            window.decorView.systemUiVisibility =
+                (View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        or View.SYSTEM_UI_FLAG_LAYOUT_STABLE)
+            @Suppress("DEPRECATION")
+            window.statusBarColor = android.graphics.Color.TRANSPARENT
+            @Suppress("DEPRECATION")
+            window.navigationBarColor = android.graphics.Color.TRANSPARENT
+        }
 
         val label = intent.getStringExtra("label") ?: "Alarm"
         val soundUriString = intent.getStringExtra("sound")
