e7fc9e4e63242fdf27ec83599a14bc146e294a5c
PranshulGG <pranshul.devmain@gmail.com>
2026-02-12 21:13:17 +0530

    updated typography to roboto flex

--- app/src/main/java/com/pranshulgg/clockmaster/ui/theme/Type.kt
+++ app/src/main/java/com/pranshulgg/clockmaster/ui/theme/Type.kt
@@ -1,34 +1,134 @@
 package com.pranshulgg.clockmaster.ui.theme
 
 import androidx.compose.material3.Typography
+import androidx.compose.ui.text.ExperimentalTextApi
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontVariation
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.pranshulgg.clockmaster.R
 
-// Set of Material typography styles to start with
-val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
+@OptIn(ExperimentalTextApi::class)
+val RobotoFlexRegular = FontFamily(
+    Font(
+        resId = R.font.roboto_flex,
+        variationSettings = FontVariation.Settings(
+            FontVariation.weight(400),
+        )
     )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
 )
+
+@OptIn(ExperimentalTextApi::class)
+val RobotoFlexMedium = FontFamily(
+    Font(
+        resId = R.font.roboto_flex,
+        variationSettings = FontVariation.Settings(
+            FontVariation.weight(500),
+        )
+    )
+)
+
+@OptIn(ExperimentalTextApi::class)
+val RobotoFlexBold = FontFamily(
+    Font(
+        resId = R.font.roboto_flex,
+        variationSettings = FontVariation.Settings(
+            FontVariation.weight(700),
+        )
+    )
+)
+
+@OptIn(ExperimentalTextApi::class)
+val RobotoFlexWide = FontFamily(
+    Font(
+        R.font.roboto_flex,
+        variationSettings = FontVariation.Settings(
+            FontVariation.weight(700),
+            FontVariation.width(125f)
+        )
+    )
+)
+
+
+val AppTypography = Typography().run {
+    copy(
+        displayLarge = displayLarge.copy(
+            fontFamily = RobotoFlexRegular,
+            fontSize = 57.sp,
+            lineHeight = 64.sp
+        ),
+        displayMedium = displayMedium.copy(
+            fontFamily = RobotoFlexRegular,
+            fontSize = 45.sp,
+            lineHeight = 52.sp
+        ),
+        displaySmall = displaySmall.copy(
+            fontFamily = RobotoFlexRegular,
+            fontSize = 36.sp,
+            lineHeight = 44.sp
+        ),
+        headlineLarge = headlineLarge.copy(
+            fontFamily = RobotoFlexRegular,
+            fontSize = 32.sp,
+            lineHeight = 40.sp
+        ),
+        headlineMedium = headlineMedium.copy(
+            fontFamily = RobotoFlexRegular,
+            fontSize = 28.sp,
+            lineHeight = 36.sp
+        ),
+        headlineSmall = headlineSmall.copy(
+            fontFamily = RobotoFlexRegular,
+            fontSize = 24.sp,
+            lineHeight = 32.sp
+        ),
+        titleLarge = titleLarge.copy(
+            fontFamily = RobotoFlexRegular,
+            fontSize = 22.sp,
+            lineHeight = 28.sp
+        ),
+        titleMedium = titleMedium.copy(
+            fontFamily = RobotoFlexMedium,
+            fontSize = 16.sp,
+            lineHeight = 24.sp
+        ),
+        titleSmall = titleSmall.copy(
+            fontFamily = RobotoFlexMedium,
+            fontSize = 14.sp,
+            lineHeight = 20.sp
+        ),
+        bodyLarge = bodyLarge.copy(
+            fontFamily = RobotoFlexRegular,
+            fontSize = 16.sp,
+            lineHeight = 24.sp
+        ),
+        bodyMedium = bodyMedium.copy(
+            fontFamily = RobotoFlexRegular,
+            fontSize = 14.sp,
+            lineHeight = 20.sp
+        ),
+        bodySmall = bodySmall.copy(
+            fontFamily = RobotoFlexRegular,
+            fontSize = 12.sp,
+            lineHeight = 16.sp
+        ),
+        labelLarge = labelLarge.copy(
+            fontFamily = RobotoFlexMedium,
+            fontSize = 14.sp,
+            lineHeight = 20.sp
+        ),
+        labelMedium = labelMedium.copy(
+            fontFamily = RobotoFlexMedium,
+            fontSize = 12.sp,
+            lineHeight = 16.sp
+        ),
+        labelSmall = labelSmall.copy(
+            fontFamily = RobotoFlexMedium,
+            fontSize = 11.sp,
+            lineHeight = 16.sp
+        ),
+    )
+}
