4da739ff460e32fb2854c16582cd2c48a110a437
PranshulGG <pranshul.devmain@gmail.com>
2026-02-12 21:07:26 +0530

    updated pomodoro setting top bar

--- app/src/main/java/com/pranshulgg/clockmaster/screens/setting_screens/pomodoro_setting_screen.kt
+++ app/src/main/java/com/pranshulgg/clockmaster/screens/setting_screens/pomodoro_setting_screen.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.LargeFlexibleTopAppBar
 import androidx.compose.material3.LargeTopAppBar
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -79,8 +80,12 @@ fun PomodoroSettings(
 
 
     Scaffold(
+        containerColor = MaterialTheme.colorScheme.surfaceContainer,
         topBar = {
-            LargeTopAppBar(
+            LargeFlexibleTopAppBar(
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.surfaceContainer
+                ),
                 title = { Text("Pomodoro") },
                 navigationIcon = {
                     IconButton(onClick = { navController.popBackStack() }) {
