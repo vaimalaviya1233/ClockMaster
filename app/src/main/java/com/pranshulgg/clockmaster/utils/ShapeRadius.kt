5b49f7281179bfa2f3d8d99c780521a239dab5b3
PranshulGG <pranshul.devmain@gmail.com>
2026-02-12 21:15:05 +0530

    added shape radius and font assets

---
+++ app/src/main/java/com/pranshulgg/clockmaster/utils/ShapeRadius.kt
@@ -0,0 +1,17 @@
+package com.pranshulgg.clockmaster.utils
+
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.ui.unit.dp
+
+object Radius {
+    val None = 0.dp
+    val ExtraSmall = 4.dp
+    val Small = 8.dp
+    val Medium = 12.dp
+    val Large = 16.dp
+    val LargeIncreased = 20.dp
+    val ExtraLarge = 28.dp
+    val ExtraLargeIncreased = 32.dp
+    val ExtraExtraLarge = 48.dp
+    val Full = 999.dp
+}
